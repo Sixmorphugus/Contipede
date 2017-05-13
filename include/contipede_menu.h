a06eeebb6b7a3c1157523955562c0e518c2a99e6
Chris <chris6smith@yahoo.co.uk>
2017-05-13 16:42:30 +0100

    Started working on menu

---
+++ include/contipede_menu.h
@@ -0,0 +1,8 @@
+#pragma once
+// Main menu stuff.
+
+void cont_menu_init();
+
+void cont_menu_update();
+void cont_menu_draw();
+void cont_menu_sendch(int ch);
---
+++ src/contipede_menu.c
@@ -0,0 +1,9 @@
+#include "contipede_menu.h"
+#include "contipede_colorpairs.h"
+
+#include "main.h"
+
+void cont_menu_init()
+{
+
+}
